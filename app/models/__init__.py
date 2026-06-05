d3240543beebddc731b77f1b58ae226cf3e1f10f
Ali Kemal Çalak <kemalcalak@gmail.com>
2026-06-05 17:57:08 +0300

    feat: add ticket data model & migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,17 @@
 from app.models.file import File
+from app.models.support import (
+    SupportMessage,
+    SupportMessageAttachment,
+    SupportTicket,
+)
 from app.models.user import User
 from app.models.user_activity import UserActivity
 
-__all__ = ["File", "User", "UserActivity"]
+__all__ = [
+    "File",
+    "SupportMessage",
+    "SupportMessageAttachment",
+    "SupportTicket",
+    "User",
+    "UserActivity",
+]
